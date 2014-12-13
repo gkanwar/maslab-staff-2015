037ab6e20aedea282dd336246ad79f4cee1b395f
Gurtej Kanwar <gurtej@mit.edu>
2014-12-13 22:15:43 +0000

    Adds some basic example files for various tasks, in C/C++

---
+++ examples/test_gpio.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "mraa.h"
+
+int main() {
+  mraa_init();
+
+  mraa_gpio_context gpio;
+  gpio = mraa_gpio_init(13);
+  if (gpio == NULL) {
+    printf("Gpio null!\n");
+    exit(1);
+  }
+  mraa_result_t r = mraa_gpio_dir(gpio, MRAA_GPIO_OUT);
+  if (r != MRAA_SUCCESS) {
+    mraa_result_print(r);
+    exit(2);
+  }
+
+  while (1) {
+    printf("Gpio high\n");
+    r = mraa_gpio_write(gpio, 1);
+    if (r != MRAA_SUCCESS) {
+      mraa_result_print(r);
+      exit(3);
+    }
+    sleep(1);
+    printf("Gpio low\n");
+    r = mraa_gpio_write(gpio, 0);
+    if (r != MRAA_SUCCESS) {
+      mraa_result_print(r);
+      exit(4);
+    }
+    sleep(1);
+  }
+
+  mraa_gpio_close(gpio);
+}
---
+++ examples/test_spi.cpp
@@ -0,0 +1,69 @@
+#include <unistd.h>
+#include <stdint.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#include "mraa.hpp"
+
+int running = 0;
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT) {
+    printf("closing spi nicely\n");
+    running = -1;
+  }
+}
+
+int main()
+{
+  signal(SIGINT, sig_handler);
+  mraa::Spi* spi = new mraa::Spi(0);
+  spi->bitPerWord(32);
+  char rxBuf[2];
+  char writeBuf[4];
+  unsigned int sensorRead = 0x20000000;
+  writeBuf[0] = sensorRead & 0xff;
+  writeBuf[1] = (sensorRead >> 8) & 0xff;
+  writeBuf[2] = (sensorRead >> 16) & 0xff;
+  writeBuf[3] = (sensorRead >> 24) & 0xff;
+  float total = 0;
+  struct timeval tv;
+  int init = 0;
+  while (running == 0) {
+    char* recv = spi->write(writeBuf, 4);
+    if (recv) {
+      unsigned int recvVal = 0 | recv[3];
+      recvVal = (recvVal << 8) | recv[2];
+      recvVal = (recvVal << 8) | recv[1];
+      recvVal = (recvVal << 8) | recv[0];
+      //printf("Received: 0x%.8x\n", recvVal);
+      // Sensor reading
+      short reading = (recvVal >> 10) & 0xffff;
+      if (init) {
+	unsigned long long ms = (unsigned long long)(tv.tv_sec)*1000 +
+		(unsigned long long)(tv.tv_usec) / 1000;
+	gettimeofday(&tv, NULL);
+	ms -= (unsigned long long)(tv.tv_sec)*1000 +
+		(unsigned long long)(tv.tv_usec) / 1000;
+	int msi = (int)ms;
+	float msf = (float)msi;
+	float rf = (float)reading;
+        total += -0.001 * msf * (rf / 80.0);
+        printf("Reading: %f, Total: %f, Time: %f\n", rf, total, -msf);
+      }
+      else {
+	init = 1;
+	gettimeofday(&tv, NULL);
+      }
+    }
+    else {
+      printf("No recv\n");
+    }
+    usleep(5000);
+  }
+
+  delete spi;
+
+  return 0;
+}
