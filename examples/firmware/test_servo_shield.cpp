77e0adfb6cf99751d07cb842011994d3dcd3e747
Gurtej Kanwar <gurtej@mit.edu>
2015-01-09 05:26:47 +0000

    Shield show now work.  Will update comments soon...

--- examples/firmware/test_servo_shield.cpp
+++ examples/firmware/test_servo_shield.cpp
@@ -5,7 +5,6 @@
 // Controls a servo on port 12 of the servo shield.
 
 #include "mraa.hpp"
-
 #include <cassert>
 #include <csignal>
 #include <iostream>
@@ -14,8 +13,9 @@
 #define SHIELD_I2C_ADDR 0x40
 
 int running = 1;
+#define MS 1000
 
-std::vector<uint8_t> registers = {
+uint8_t registers[] = {
   6,   // output 0
   10,  // output 1
   14,  // output 2
@@ -43,38 +43,53 @@ void sig_handler(int signo)
   }
 }
 
+void initPWM(mraa::I2c *i2c) {
+  char writeBuf[2] = {0};
+  writeBuf[0] = 0x00; // Write to MODE 1 Register;
+  writeBuf[1] = 1 << 4; // Enable Sleep Mode
+ 
+  i2c->address(SHIELD_I2C_ADDR);
+  i2c->write(writeBuf, 2);
+ 
+  usleep(10 * MS);
+
+  writeBuf[0] = 0xFE; // Write Prescaler Register
+  writeBuf[1] = 0xA3; // Set pwm frequency to ~40 Hz
+
+  i2c->address(SHIELD_I2C_ADDR);
+  i2c->write(writeBuf, 2);
+  
+  writeBuf[0] = 0; // Write to the MODE 1 register
+  writeBuf[1] = 1 << 5 // Enable auto increment mode
+	    | 0 << 4; // Enable the oscillator
+        
+  i2c->address(SHIELD_I2C_ADDR);
+  i2c->write(writeBuf, 2);
+}
+
+
 void writePWM(mraa::I2c* i2c, int index, double duty) {
   assert(0.0 <= duty && duty <= 1.0);
   assert(0 <= index && index < 16);
-  double on = 4096.0 * duty;
+  double on = 4095.0 * duty;
   uint16_t onRounded = (uint16_t) on;
-  uint16_t offRounded = 4096 - onRounded;
+  uint16_t offRounded = 4095 - onRounded;
 
-  char writeBuf[2];
+  char writeBuf[5];
 
   // ON_L
   writeBuf[0] = registers[index];
-  writeBuf[1] = onRounded & 0xff;
+  writeBuf[1] = 0x00; // ON LSB
+  writeBuf[2] = 0x00; // ON MSB
+  writeBuf[3] = onRounded & 0xFF; // OFF LSB
+  writeBuf[4] = (onRounded >> 8) & 0xFF; // OFF MSB
   i2c->address(SHIELD_I2C_ADDR);
-  i2c->write(writeBuf, 2);
+  i2c->write(writeBuf, 5);
+}
 
-  // ON_H
-  writeBuf[0] = registers[index] + 1;
-  writeBuf[1] = (onRounded >> 8) & 0xff;
-  i2c->address(SHIELD_I2C_ADDR);
-  i2c->write(writeBuf, 2);
 
-  // OFF_L
-  writeBuf[0] = registers[index] + 2;
-  writeBuf[1] = offRounded & 0xff;
-  i2c->address(SHIELD_I2C_ADDR);
-  i2c->write(writeBuf, 2);
-
-  // OFF_H
-  writeBuf[0] = registers[index] + 3;
-  writeBuf[1] = (offRounded >> 8) & 0xff;
-  i2c->address(SHIELD_I2C_ADDR);
-  i2c->write(writeBuf, 2);
+void setServoPosition(mraa::I2c *i2c, int index, double duty) {
+  writePWM(i2c, index, .04 * duty + .02);
 }
 
 int main()
@@ -83,14 +98,18 @@ int main()
   signal(SIGINT, sig_handler);
 
   // Edison i2c bus is 6
-  mraa::I2c* i2c = new mraa::I2c(6);
-  assert(i2c != nullptr);
+  mraa::I2c* i2c = new mraa::I2c(1);
+  assert(i2c != NULL);
+
+  initPWM(i2c);
 
   while (running) {
     // Alternate two locations with 2-sec delay
-    writePWM(i2c, 12, 0.2);
+    setServoPosition(i2c, 0, 0.2);
+    setServoPosition(i2c, 1, 0.2);
     sleep(2.0);
-    writePWM(i2c, 12, 0.8);
+    setServoPosition(i2c, 0, 0.8);
+    setServoPosition(i2c, 1, 0.8);
     sleep(2.0);
   }
 }
